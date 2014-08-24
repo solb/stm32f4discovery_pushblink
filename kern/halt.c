0bb2ba175ed6870ae2b38764690b2dfe2a4fe40e
Sol Boucher <slb1566@rit.edu>
2014-08-24 05:40:46 -0400

    Use strcpy to demonstrate that library functions are working

--- kern/halt.c
+++ kern/halt.c
@@ -1,10 +1,18 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 void SystemInit(void) {}
 
 int main(void) {
-	printf("Hello, world!\n");
+	const char literal[] = "This is a sample string.";
+	char figurative[sizeof literal];
+	strcpy(figurative, literal);
+	puts(literal);
+	puts(figurative);
+	++figurative[0];
+	puts(literal);
+	puts(figurative);
 
 	while(true);
 }
